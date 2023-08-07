397bfc2061577100f4780b9c103b6442b5e4da9f
Leonardo H. Felde <leonardohfelde@gmail.com>
2023-08-06 23:01:55 -0300

    feat: criados arquivos de api
    criadas todas as chamadas de endpoints

--- api/index.js
+++ api/index.js
@@ -4,15 +4,16 @@ const AppURL = 'https://demometaway.vps-kinghost.net:8485/api'
 const storedUser = JSON.parse(localStorage.getItem('userData'))
 const token = storedUser?.accessToken
 
-const createAPI = (routeURL) => {
+const createAPI = (routeURL, responseType = 'json') => {
   const baseURL  = AppURL + routeURL
   const headers = routeURL !== '/auth'
     ? { Authorization: `Bearer ${token}` }
     : {}
-
+  
   const api = axios.create({
     baseURL,
-    headers
+    headers,
+    responseType
   })
 
   return api
