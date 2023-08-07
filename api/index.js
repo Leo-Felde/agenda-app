d645f8916259fc2a6f3f3400300d6814ef276c0a
Leonardo H. Felde <leonardohfelde@gmail.com>
2023-08-07 14:59:48 -0300

    feat: interceptador de falha de permissão

--- api/index.js
+++ api/index.js
@@ -1,20 +1,32 @@
 import axios from 'axios'
+
 const AppURL = 'https://demometaway.vps-kinghost.net:8485/api'
 
 const storedUser = JSON.parse(localStorage.getItem('userData'))
 const token = storedUser?.accessToken
 
 const createAPI = (routeURL, responseType = 'json') => {
-  const baseURL  = AppURL + routeURL
+  const baseURL = AppURL + routeURL
   const headers = routeURL !== '/auth'
     ? { Authorization: `Bearer ${token}` }
     : {}
-  
+
   const api = axios.create({
     baseURL,
     headers,
-    responseType
+    responseType,
   })
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response.status === 401) {
+        localStorage.removeItem('userData')
+        return navigateTo('/auth')
+      }
+      return Promise.reject(error)
+    }
+  )
 
   return api
 }
