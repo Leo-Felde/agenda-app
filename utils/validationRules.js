af358221a1302670048ca81638e92570c90082b6
Leonardo H. Felde <leonardohfelde@gmail.com>
2023-08-07 23:17:06 -0300

    chore: validacao movida para utils + formatacao

--- utils/validationRules.js
+++ utils/validationRules.js
@@ -6,5 +6,13 @@ export const rules = ref({
   email: value => {
     const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     return pattern.test(value) || 'E-mail inválido.'
+  },
+  cpf: value => {
+    const pattern = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/
+    return pattern.test(value) || 'Cpf inválido'
+  },
+  senha: value => {
+    const pattern = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d!@#$%^&*()-_=+[\]{}|;:'",.<>/?\\]+$/
+    return value.length >= 8 ? pattern.test(value) || 'Deve conter letras e numeros' : 'Deve ter no minimo 8 characteres'
   }
 })
